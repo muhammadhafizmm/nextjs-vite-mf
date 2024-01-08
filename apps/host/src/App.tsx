1b07b1cf12bffc237adc09110ad3e68acc850458
muhammadhafizmm <m.hafizmaulana777@gmail.com>
2024-01-08 11:41:02 +0700

    feat(init-nextjs-vite-mf): update deps and apply remote app provider

--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -8,40 +8,45 @@ function App() {
   const [count, setCount] = useState(0);
   const CustomButton = lazy(() => import("remote/components/CustomButton"));
   const TestButton = lazy(() => import("efi_remote/components/TestButton"));
+  const RemoteAppProvider = lazy(
+    () => import("efi_remote/features/RemoteAppProvider")
+  );
   const ButtonBox = lazy(() => import("efi_remote/components/ButtonBox"));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <Suspense fallback="loading">
+      <RemoteAppProvider>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+          <Suspense fallback={<div>Loading...</div>}>
+            <CustomButton />
+          </Suspense>
+          <Suspense fallback={<div>Loading...</div>}>
+            <TestButton />
+          </Suspense>
+          {/* <Suspense fallback={<div>Loading...</div>}>
+              <ButtonBox />
+            </Suspense> */}
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-        <Suspense fallback={<div>Loading...</div>}>
-          <CustomButton />
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <TestButton />
-        </Suspense>
-        {/* <Suspense fallback={<div>Loading...</div>}>
-          <ButtonBox />
-        </Suspense> */}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </RemoteAppProvider>
+    </Suspense>
   );
 }
 
